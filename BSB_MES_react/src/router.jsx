fdc35e84a90b7fb783de6b630658a631abaab473
Diego Etcharren <diegoe96@gmail.com>
2026-01-20 23:03:35 -0600

    Router new file

--- BSB_MES_react/src/router.jsx
+++ BSB_MES_react/src/router.jsx
@@ -1,9 +1,10 @@
 import {createBrowserRouter} from 'react-router-dom'
+import Layout from './layouts/Layout';
 
 const router = createBrowserRouter([
     {
         path : '/',
-        element :
+        element : <Layout />
     }
 ])
 
