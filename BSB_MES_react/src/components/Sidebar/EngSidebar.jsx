ba647f670ec94980ade51a446b63a2b1dad5e7e2
Diego Etcharren <diegoe96@gmail.com>
2026-02-03 20:28:26 -0600

    Changed buttons to Links on navigation bar in sidebar.

--- BSB_MES_react/src/components/Sidebar/EngSidebar.jsx
+++ BSB_MES_react/src/components/Sidebar/EngSidebar.jsx
@@ -1,8 +1,10 @@
+import { Link, useLocation } from 'react-router-dom';
 import useMES from "../../hooks/useMES"
 import BrandLogo from "../../components/BrandLogo";
 
 export default function EngSidebar() {
-  const {activeTab, handleActiveTab} = useMES();
+  const location = useLocation();
+
   return (
     <div className="w-64 bg-white border-r border-border-subtle flex flex-col z-20">
       <div className="p-6 flex items-center gap-3">
@@ -10,38 +12,34 @@ export default function EngSidebar() {
         <p className="font-black text-xl tracking-tight text-charcoal">ADMIN</p>
       </div>
       <nav className="flex-1 px-3 mt-4 space-y-1">
-        <button
-          className={`nav-link active flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full ${activeTab === "dashboard" ? "active" : ""}`}
-          type="button"
-          onClick={() => handleActiveTab("dashboard")}
+        <Link
+          to="/eng/dashboard"
+          className={`nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full ${location.pathname === "/eng/dashboard" ? "active" : ""}`}
         >
           <span className="material-symbols-outlined">dashboard</span>
           <p className="text-sm">Dashboard</p>
-        </button>
-        <button
-          className="nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full"
-          type="button"
-          onClick={() => handleActiveTab("orders")}
+        </Link>
+        <Link
+          to="/eng/orders"
+          className={`nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full ${location.pathname === "/eng/orders" ? "active" : ""}`}
         >
           <span className="material-symbols-outlined">assignment</span>
           <p className="text-sm">Orders</p>
-        </button>
-        <button
-          className="nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full"
-          type="button"
-          onClick={() => handleActiveTab("orders")}
+        </Link>
+        <Link
+          to="/eng/operators"
+          className={`nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full ${location.pathname === "/eng/operators" ? "active" : ""}`}
         >
           <span className="material-symbols-outlined">person</span>
           <p className="text-sm">Operators</p>
-        </button>
-        <button
-          className="nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full"
-          type="button"
-          onClick={() => handleActiveTab("operators")}
+        </Link>
+        <Link
+          to="/eng/products"
+          className={`nav-link flex items-center gap-3 px-4 py-3 rounded transition-all duration-200 cursor-pointer w-full ${location.pathname === "/eng/products" ? "active" : ""}`}
         >
           <span className="material-symbols-outlined">inventory_2</span>
           <p className="text-sm">Products</p>
-        </button>
+        </Link>
       </nav>
     </div>
   );
