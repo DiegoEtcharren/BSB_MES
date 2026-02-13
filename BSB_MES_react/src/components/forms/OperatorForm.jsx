a74e2641517d9cf54d69bb662df82fe5100e92d6
Diego Etcharren <diegoe96@gmail.com>
2026-02-12 19:48:05 -0600

    Fixed className inputs on operator forms

--- BSB_MES_react/src/components/forms/OperatorForm.jsx
+++ BSB_MES_react/src/components/forms/OperatorForm.jsx
@@ -111,7 +111,7 @@ export default function OperatorForm() {
               </span>
               <input
                 ref={emailRef}
-                className="w-full pl-10 pr-4 py-2.5 rounded-lg border-slate-300 focus:border-primary focus:ring focus:ring-primary/20 transition-shadow text-sm placeholder:text-slate-400"
+                className="w-full pl-10 pr-4 py-2.5 rounded-lg border border-slate-300 focus:outline-primary focus:border-primary focus:outline-none focus:ring focus:ring-primary/20  transition-colors duration-200  text-sm placeholder:text-slate-400"
                 id="email"
                 placeholder="name@bsbsystems.com"
                 type="email"
@@ -128,7 +128,7 @@ export default function OperatorForm() {
             <div className="relative">
               <select
                 ref={roleRef}
-                className="w-full px-4 py-2.5 rounded-lg border-slate-300 focus:border-primary focus:ring focus:ring-primary/20 transition-shadow text-sm text-charcoal appearance-none bg-white"
+                className="w-full px-4 py-2.5 rounded-lg border border-slate-300 focus:outline-primary focus:border-primary focus:outline-none focus:ring focus:ring-primary/20  transition-colors duration-200  text-sm placeholder:text-slate-400 text-charcoal appearance-none bg-white"
                 id="accessLevel"
               >
                 <option value="">Select a role...</option>
