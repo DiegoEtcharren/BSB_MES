1bca38943b15b4bcf1c6a24419d753bda0a20a93
Diego Etcharren <diegoe96@gmail.com>
2026-05-12 16:01:36 -0600

    Added description to certificates

--- BSB_MES_Laravel/database/seeders/CertificateSeeder.php
+++ BSB_MES_Laravel/database/seeders/CertificateSeeder.php
@@ -14,11 +14,11 @@ class CertificateSeeder extends Seeder
     public function run(): void
     {
         $certificates = [
-            ['name' => 'BTC', 'description' => '', 'is_active' => true],
-            ['name' => 'MILL', 'description' => '', 'is_active' => true],
-            ['name' => 'C OF C', 'description' => '', 'is_active' => true],
-            ['name' => 'ASME', 'description' => '', 'is_active' => true],
-            ['name' => 'CE', 'description' => '', 'is_active' => true],
+            ['name' => 'BTC', 'description' => 'Burst Test Certificate', 'is_active' => true],
+            ['name' => 'MILL', 'description' => 'Material MILL Certificate', 'is_active' => true],
+            ['name' => 'C OF C', 'description' => 'Certifcate of Conformance', 'is_active' => true],
+            ['name' => 'ASME', 'description' => 'American Society of Mechanical Engineers', 'is_active' => true],
+            ['name' => 'CE', 'description' => 'Conformité Européenne', 'is_active' => true],
         ];
 
         DB::table('certificates')->insert($certificates);
